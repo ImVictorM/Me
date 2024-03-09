a2d0cfbcb591a6bc516e172a493eaad4893c8a08
Victor Mendes <victorutdsim7@gmail.com>
2024-03-09 14:41:39 -0300

    setup styles and font

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,14 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    colors: {
+      chineseBlack: "#141813",
+      electricGreen: "#00EE00",
+      jetStream: "#BFDABF"
+    },
+    fonts: {
+      monofonto: "monofonto",
+    }
+  }
+}
---
+++ src/styles/theme.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+  colors: {
+    chineseBlack: "#141813",
+    electricGreen: "#00EE00",
+    jetStream: "#BFDABF"
+  },
+  fonts: {
+    monofonto: "monofonto",
+  }
+}
