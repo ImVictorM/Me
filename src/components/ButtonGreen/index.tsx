1b6ed63fc6c19785e144c75f8fec9a3172a9a6dd
Victor Mendes <victorutdsim7@gmail.com>
2024-03-29 17:23:13 -0300

    feat: loading button when sending email

--- src/components/ButtonGreen/index.tsx
+++ src/components/ButtonGreen/index.tsx
@@ -1,15 +1,32 @@
 import { ButtonHTMLAttributes } from "react";
-import { StyledGreenButton } from "./style";
+import { LoadingDot, StyledGreenButton } from "./style";
 
 type ButtonGreenProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   children: string;
+  isLoading: boolean;
+  loadingText: string;
 };
 
 export default function ButtonGreen({
   children,
+  isLoading,
+  loadingText,
   ...defaultButtonProps
 }: ButtonGreenProps) {
   return (
-    <StyledGreenButton {...defaultButtonProps}>{children}</StyledGreenButton>
+    <StyledGreenButton {...defaultButtonProps}>
+      {isLoading ? (
+        <div className="loading-wrapper">
+          <span className="loading-text">{loadingText || "LOADING"}</span>
+          <div>
+            <LoadingDot animationDelay="0.1s" />
+            <LoadingDot animationDelay="0.2s" />
+            <LoadingDot animationDelay="0.3s" />
+          </div>
+        </div>
+      ) : (
+        children
+      )}
+    </StyledGreenButton>
   );
 }
